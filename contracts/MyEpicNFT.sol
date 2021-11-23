178f73a66b7fe0872c883a4e66aa3bc4dfd67f5c
lebonYG <lebonganza@outlook.com>
2021-11-23 10:13:50 -0500

    Refactored nft state

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 import "../libraries/Base64.sol";
+import "../libraries/Svg.sol";
 
 // We inherit the contract we imported. This means we'll have access
 // to the inherited contract's methods.
@@ -14,7 +15,6 @@ contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: green; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
     string[] firstWords = ["Achille", "Ceres", "Chia", "Danu", "Cepheus", "Leto", "Thanathos", "Tyr"];
 
@@ -22,7 +22,12 @@ contract MyEpicNFT is ERC721URIStorage {
 
     string[] thirdWords = ["Mercury" ,"Venus" ,"Earth" ,"Mars", "Jupiter" ,"Saturn" ,"Uranus" ,"Neptune"];
 
-    event NewEpicNFTMinted(address sender, uint256 tokenId);
+    string[] list1 = ["Pink", "Crimson", "Red", "Maroon", "Brown", "Mist", "Rose", "Salmon", "Coral", "Orange-Red", "Chocolate", "Orange", "Gold", "Tan", "Beige"];
+
+    string[] list2 = ["Lime", "Green", "Spring-green", "Aquamarine", "Turquoise", "Azure", "Aqua", "Teal", "Lavender", "Blue", "Navy", "Blue-Violet", "Indigo", "White", "Black"];
+
+    //Notify the client 
+    event NewEpicNFTMinted(address sender, uint256 tokenId, uint256 mintCount);
 
     // We need to pass the name of our NFTs token and it's symbol.
     constructor() ERC721 ("SquareNFT", "SQUARE") {
@@ -52,6 +57,22 @@ contract MyEpicNFT is ERC721URIStorage {
         return thirdWords[rand];
     }
 
+    // Create a function to randomly pick a color from each array.
+    function pickRandomFirstColor(uint256 tokenId) public view returns (string memory) {
+        // Seed the random generator
+        uint256 rand = random(string(abi.encodePacked("FIRST_COLOR", Strings.toString(tokenId))));
+        // Squash the # between 0 and the length of the array to avoid going out of bounds.
+        rand = rand % list1.length;
+        return list1[rand];
+    }
+    function pickRandomSecondColor(uint256 tokenId) public view returns (string memory) {
+        // Seed the random generator
+        uint256 rand = random(string(abi.encodePacked("SECOND_COLOR", Strings.toString(tokenId))));
+        // Squash the # between 0 and the length of the array to avoid going out of bounds.
+        rand = rand % list2.length;
+        return list2[rand];
+    }
+
     function random(string memory input) internal pure returns (uint256) {
         return uint256(keccak256(abi.encodePacked(input)));
     }
@@ -69,7 +90,10 @@ contract MyEpicNFT is ERC721URIStorage {
         string memory thirdWord = pickRandomThirdWord(newItemId);
         string memory combinedWord = string(abi.encodePacked(firstWord, secondWord, thirdWord));
 
-        string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, '</text></svg>'));
+        string memory firstColor = pickRandomFirstColor(newItemId);
+        string memory secondColor = pickRandomSecondColor(newItemId);
+
+        string memory finalSvg = Svg.generate(firstColor, secondColor);
 
         // Get all the JSON metadata in place and base64 encode it.
         string memory json = Base64.encode(
@@ -105,11 +129,6 @@ contract MyEpicNFT is ERC721URIStorage {
 
         _tokenIds.increment();
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
-        emit NewEpicNFTMinted(msg.sender, newItemId);
-    }
-
-    function getTotalNFTsMinted() public view returns(uint256){
-        uint256 count = _tokenIds.current();
-        return count;
+        emit NewEpicNFTMinted(msg.sender, newItemId, newItemId);
     }
 }
