dc36bf9db7cea700bb78f32295b3a70839b191bc
lebonYG <lebonganza@outlook.com>
2021-11-23 22:18:06 -0500

    Refactored deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config()
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,11 +19,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: {version:"0.8.0"},
   networks: {
     rinkeby: {
       url: process.env.API_KEY,
       accounts: [process.env.ACCOUNT_KEY]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
-};
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,17 +3,6 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
-  
-    // Call the function.
-    let txn = await nftContract.makeAnEpicNFT()
-    // Wait for it to be mined.
-    await txn.wait()
-    console.log("Minted NFT #1")
-  
-    txn = await nftContract.makeAnEpicNFT()
-    // Wait for it to be mined.
-    await txn.wait()
-    console.log("Minted NFT #2")
   };
   
   const runMain = async () => {
